1fcb339802e7491624c30421c18abb8bc6c83c7f
footcha <petr.kozelek@gmail.com>
2019-05-10 21:16:59 +0200

    API docs for MessageHandler

--- src/Oriflame.RedisMessaging.ReliableDelivery/Subscribe/IMessageHandler.cs
+++ src/Oriflame.RedisMessaging.ReliableDelivery/Subscribe/IMessageHandler.cs
@@ -3,11 +3,32 @@ using StackExchange.Redis;
 
 namespace Oriflame.RedisMessaging.ReliableDelivery.Subscribe
 {
+    /// <summary>
+    /// Represents logic for processing received messages, both expected and unexpected (e.g. out of order)
+    /// </summary>
     public interface IMessageHandler
     {
-        RedisChannel Channel { get; }
+        /// <summary>
+        /// name of a channel from that a message is received and processed
+        /// </summary>
+        string Channel { get; }
+
+        /// <summary>
+        /// Processes a received message
+        /// </summary>
+        /// <param name="message">received message that should be processed</param>
         void HandleMessage(Message message);
+        
+        /// <summary>
+        /// Check whether there are some messages in Redis server pending,
+        /// i.e. they were not received and handled by this <see cref="IMessageHandler"/>
+        /// </summary>
         void CheckMissedMessages();
+
+        /// <summary>
+        /// Date at which last message was processed either vua <see cref="HandleMessage"/>
+        /// or <see cref="CheckMissedMessages"/>
+        /// </summary>
         DateTime LastActivityAt { get; }
     }
 }
